fd5ea511e171cf1051a5971eb941ff3eac936bfc
Anna3001 <aniaw3001@icloud.com>
2022-08-19 16:00:40 +0200

    Adding field

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,18 +1,26 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    static int block_size = 10;
+    int width = 100;
+    int height = 70;
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         VBox root = new VBox(10);
+        root.setPadding(new Insets(8));
+
+        Field field = new Field(width, height);
+        root.getChildren().add(field);
+
         Scene scene = new Scene(root);
-        root.setPrefSize(1000,700);
-        root.setStyle("-fx-background-color: lightgrey;");
 
         stage.setResizable(false);
         stage.setScene(scene);
